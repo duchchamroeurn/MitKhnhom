05010239a2d9342b5da6555dafd9f3b94e289c87
dchamroeun <chamroeurn.duch@gmail.com>
2018-09-06 14:20:56 +0700

    no message

---
+++ MitKhnhom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MitKhnhom
+//
+//  Created by iOSDev on 6/9/18.
+//  Copyright © 2018 iOSDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
